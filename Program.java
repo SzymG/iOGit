8de5b5b1fecf4ec72db434b275a894bd24567448
unknown <inf132225@lab-44-13.cs.put.poznan.pl>
2018-10-29 14:10:08 +0100

    the commit message for your changes. Lines starting

--- Program.java
+++ Program.java
@@ -2,16 +2,19 @@ class Lamp {
 
   // instance variable
   private boolean isOn;
+  private boolean isOff;
 
   // method
   public void turnOn() {
     isOn = true;
 	System.out.println("Nowa");
+		System.out.println("Eeeeeeee");
   }
 
   // method
   public void turnOff() {
   	isOn = false;
 	System.out.println("Last commit");
+	System.out.println("Eeeeeeee");
   }
 }
